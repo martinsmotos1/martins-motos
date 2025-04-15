dadcdee1b6dc6767b6d35cd0cb4ac5835f8e2886
Victor Gabriel <victorgom504@gmail.com>
2025-04-15 17:46:52 -0300

    Versão final com painel admin

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,3 @@
+export default function Admin() {
+  return <h1>Painel Administrativo - Acesso restrito</h1>;
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1>Bem-vindo à loja Martins Motos!</h1>;
+}
